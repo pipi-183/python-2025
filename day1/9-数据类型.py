eefed68aa42c7cc6054c6fff2d925f0ca47daa47
皮皮 <a1831564268@163.com>
2025-01-17 13:49:34 +0800

    day1

---
+++ day1/9-数据类型.py
@@ -0,0 +1,29 @@
+def use_hex():
+    a = 123
+    print(bin(a))  # 功能 将整数转换为二进制
+    print(hex(a))  # 功能 将整数转换为十六进制
+    print(oct(a))  # 功能 将整数转换为八进制
+
+    b = -5
+
+    print(bin(b))
+
+
+def use_float():
+    f = 1.234567891234567891
+    print(f)
+
+
+def use_bool():
+    print(True+1)
+    print(False+1)
+
+
+def use_complex():
+    c = complex(3, 4)
+    print("c is %d+%dj" % (c.real, c.imag))
+
+# use_hex()
+# use_float()
+# use_bool()
+use_complex()
