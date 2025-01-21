aa0387c9ce96e54e3c878937945cc912e4656d8f
皮皮 <a1831564268@163.com>
2025-01-21 13:51:14 +0800

    .

---
+++ day5/13-单例模式.py
@@ -0,0 +1,20 @@
+class MusicPlayer(object):
+    instance = None  # 用来保存对象的
+
+    def __new__(cls, *args, **kwargs):
+        # 1、创建对象，分配空间
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)  # 父亲的new类似于C的malloc
+        return cls.instance
+
+    def __init__(self, name):
+        self.name = name
+
+
+if __name__ == '__main__':
+    player1 = MusicPlayer('七里香')
+    player2 = MusicPlayer('东风破')
+    print(id(player1))
+    print(id(player2))
+    print(player1.name)
+    print(player2.name)
---
+++ day5/7-多重继承.py
@@ -0,0 +1,25 @@
+class A:
+    def test(self):
+        print('A test')
+
+    def demo(self):
+        print('A demo')
+
+
+class B:
+    def test(self):
+        print('B test')
+
+    def demo(self):
+        print('B demo')
+
+
+class C(A, B):
+    def test(self):
+        print('C test')
+
+
+if __name__ == '__main__':
+    c = C()
+    c.test()
+    print(C.__mro__)
